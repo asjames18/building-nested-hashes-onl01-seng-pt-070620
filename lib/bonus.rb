b0fc2fa54d2ad48ccb7acc1b477a17cf4493c64e
Antonio James <asjames18@gmail.com>
2020-07-16 14:42:36 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -3,8 +3,9 @@ def bonus
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive
-  :hero_friends => [
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      epic_tragedy = { hero: { status: "dead"}}
+      :hero_friends => [
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
