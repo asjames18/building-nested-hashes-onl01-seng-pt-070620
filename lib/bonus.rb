52dab75dab81b4dd8c95f3fa3d0695daa8d663ad
Antonio James <asjames18@gmail.com>
2020-07-16 14:53:14 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-  #code your solution h      [:montague][:hero][:status] = "dead"
-  epic_tragedy[:montague][:hero][:status] == "dead"
+  #code your solution       [:montague][:hero][:status] = "dead"
+  puts epic_tragedy[:montague][:hero][:status] = "dead"
 
 
 
